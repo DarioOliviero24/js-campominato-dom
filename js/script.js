fed7495f92b8388ca2f07e9b8d542f58960fed67
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-07-29 18:38:14 +0200

    Salvato varianti e le ho divise

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const Facile = document.getElementById('Facile');
+const Medio = document.getElementById('Medio');
+const Difficile = document.getElementById('Difficile');
+//FACILE
+
+Facile.addEventListener('click', function (){
+    console.log();
+})
+
+//MEDIO
+
+
+Medio.addEventListener('click', function (){
+    console.log();
+})
+//DIFFICILE
+
+
+Difficile.addEventListener('click', function (){
+    console.log();
+})
