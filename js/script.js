033fc30856a4c127ff58d0aa69d3294d8d4280ac
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-07-29 19:02:34 +0200

    aggiunto js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,46 @@
 const Facile = document.getElementById('Facile');
 const Medio = document.getElementById('Medio');
 const Difficile = document.getElementById('Difficile');
+
 //FACILE
 
 Facile.addEventListener('click', function (){
-    console.log();
+    console.log('Clicco sul bottone Facile');
+    for( let i = 0; i <= 100, i++){
+        console.log(i);
+
+        const newcell = document.createElement('div');
+        newcell.innerHTML - i;
+        newcell.append(Facile);
+    }
+        
 })
 
 //MEDIO
 
 
 Medio.addEventListener('click', function (){
-    console.log();
+    console.log('Clicco sul bottone Medio');
+    for( let j = 0; j <= 81, j++){
+        console.log(j);
+
+        const newcell = document.createElement('div');
+        newcell.innerHTML - j;
+        newcell.append(Medio);
+    }
+        
 })
 //DIFFICILE
 
 
 Difficile.addEventListener('click', function (){
-    console.log();
+    console.log('Clicco sul bottone Difficile');
+    for( let k = 0; k <= 100, k++){
+        console.log(k);
+
+        const newcell = document.createElement('div');
+        newcell.innerHTML - k;
+        newcell.append(Difficile);
+    }
+        
 })
