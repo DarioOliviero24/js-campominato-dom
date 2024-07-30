ce72d0179a82bdb18c68455e338509039070e926
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-07-30 13:27:08 +0200

    creazione button-facile

--- js/script.js
+++ js/script.js
@@ -1,46 +1,15 @@
 const Facile = document.getElementById('Facile');
-const Medio = document.getElementById('Medio');
-const Difficile = document.getElementById('Difficile');
 
 //FACILE
 
 Facile.addEventListener('click', function (){
-    console.log('Clicco sul bottone Facile');
-    for( let i = 0; i <= 100, i++){
-        console.log(i);
+    const container = document.getElementById('container');
+    
+    for (let i = 1; i <= 100, i++){
 
         const newcell = document.createElement('div');
-        newcell.innerHTML - i;
-        newcell.append(Facile);
-    }
-        
-})
-
-//MEDIO
-
-
-Medio.addEventListener('click', function (){
-    console.log('Clicco sul bottone Medio');
-    for( let j = 0; j <= 81, j++){
-        console.log(j);
-
-        const newcell = document.createElement('div');
-        newcell.innerHTML - j;
-        newcell.append(Medio);
-    }
-        
-})
-//DIFFICILE
-
-
-Difficile.addEventListener('click', function (){
-    console.log('Clicco sul bottone Difficile');
-    for( let k = 0; k <= 100, k++){
-        console.log(k);
-
-        const newcell = document.createElement('div');
-        newcell.innerHTML - k;
-        newcell.append(Difficile);
+        newcell.innerHTML = i;
+        container.append(newcell);
     }
         
 })
