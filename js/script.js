65700a2c76443bfba166ea2208d0edb318b40bf3
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-07-30 14:10:14 +0200

    create le 3 difficolta+ il click sulla casella

--- js/script.js
+++ js/script.js
@@ -1,15 +1,55 @@
-const Facile = document.getElementById('Facile');
-
 //FACILE
+const Facile = document.getElementById('Facile');
 
 Facile.addEventListener('click', function (){
     const container = document.getElementById('container');
     
-    for (let i = 1; i <= 100, i++){
+    for (let i = 1; i <= 100; i++){
 
         const newcell = document.createElement('div');
         newcell.innerHTML = i;
-        container.append(newcell);
+        newcell.addEventListener('click', function(){
+        newcell.classList.add('dario')    
+        });
+        container.append(newcell);    
     }
+    
+});
+
+
+//Medio
+const Medio = document.getElementById('Medio');
+
+Medio.addEventListener('click', function (){
+    const container = document.getElementById('container');
+    
+    for (let i = 1; i <= 81; i++){
+
+        const newcell = document.createElement('div');
+        newcell.innerHTML = i;
+        newcell.addEventListener('click', function(){
+        newcell.classList.add('dario')    
+        });
+        container.append(newcell);
         
-})
+    }
+    
+});
+
+
+//Difficile
+const Difficile = document.getElementById('Difficile');
+
+Difficile.addEventListener('click', function (){
+    const container = document.getElementById('container');
+    
+    for (let i = 1; i <= 49; i++){
+
+        const newcell = document.createElement('div');
+        newcell.innerHTML = i;
+        newcell.addEventListener('click', function(){
+        newcell.classList.add('dario')    
+        });
+        container.append(newcell);
+    } 
+});
